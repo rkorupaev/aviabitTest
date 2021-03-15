656c8f4ad2413a06453c367001a7ba2f5b53e2b4
Roman Korupaev <rkorupaev@gmail.com>
2021-03-15 22:32:40 +0300

    estimated and actual time added

--- src/components/mainBlock/Statistics/Stats.jsx
+++ src/components/mainBlock/Statistics/Stats.jsx
@@ -3,25 +3,39 @@ import style from "./Stats.module.css"
 
 const Stats = (props) => {
 
-    let totalHours = 0;
-    let totalMinutes = 0;
     let totalTime = 0;
 
-    let filteredArray = [];
+    const countHours = (array) => {
+        totalTime = 0;
+        array.forEach(item => {
+            totalTime = totalTime + item.timeWork;
+        });
+        return totalTime;
+    };
+
+    let totalHoursAct = 0;
+    let totalMinutesAct = 0;
+
+    let totalHoursEst = 0;
+    let totalMinutesEst = 0;
+
+    let filteredArrayEst = [];
 
     if (props.flightsArray.length > 0) {
 
         props.flightsArray.forEach(item => {
             if (item.dateFlight.includes(props.filterYear)) {
-                filteredArray.push(item);
+                filteredArrayEst.push(item);
             }
         });
 
-        filteredArray.forEach(item => {
-            totalTime = totalTime + item.timeWork;
-        });
-        totalHours = Math.floor(totalTime / 3600);
-        totalMinutes = (totalTime % 3600) / 60;
+        totalHoursEst = Math.floor(countHours(filteredArrayEst) / 3600);
+        totalMinutesEst = (countHours(filteredArrayEst) % 3600) / 60;
+
+        let filteredArrayAct = filteredArrayEst.filter(item => item.type === 0);
+
+        totalHoursAct = Math.floor(countHours(filteredArrayAct) / 3600);
+        totalMinutesAct = (countHours(filteredArrayAct) % 3600) / 60;
     }
 
     let newPostTextInput = React.createRef();
@@ -31,21 +45,24 @@ const Stats = (props) => {
             <div className={style.filter}>
                 <p className={style.filter__text}>Введите год поиска:</p>
                 <input className={style.filter__input} placeholder="2017" ref={newPostTextInput}
-                          onChange={() => props.onTextAreaChange(newPostTextInput.current.value)}></input>
-                <button className={style.filter__button} onClick={()=> props.onSendQueryButtonCLick(newPostTextInput.current.value)}>Отправить запрос</button>
+                       onChange={() => props.onTextAreaChange(newPostTextInput.current.value)}></input>
+                <button className={style.filter__button}
+                        onClick={() => props.onSendQueryButtonCLick(newPostTextInput.current.value)}>Отправить запрос
+                </button>
             </div>
             <div className={style.statistics}>
                 <div className={style.statBlock}>
-                    <h2>За год</h2>
-                    <p className={style.statBlock__hours}><span>{totalHours}</span> ч. <span>{totalMinutes}</span> мин.
+                    <h2>За год:</h2>
+                    <p className={style.statBlock__hours}>Плановое
+                        время: <span>{totalHoursEst}</span> ч. <span>{totalMinutesEst}</span> мин.
                     </p>
-                    <ul><li>Тут будет список месяцев</li></ul>
+                    <p className={style.statBlock__hours}>Фактическое
+                        время: <span>{totalHoursAct}</span> ч. <span>{totalMinutesAct}</span> мин.
+                    </p>
+                    <ul>
+                        <li>Тут будет список месяцев</li>
+                    </ul>
                 </div>
-                {/*<div className={style.statBlock}>*/}
-                {/*    <h2>За месяц</h2>*/}
-                {/*    <p className={style.statBlock__hours}><span>{totalHours}</span> ч. <span>{totalMinutes}</span> мин.*/}
-                {/*    </p>*/}
-                {/*</div>*/}
             </div>
         </div>
     );
